46f66f17488a5eb84d5ff3ecdb30c414f45d84bd
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-02-18 18:14:14 +0700

    a

--- parallelfor/c.c
+++ parallelfor/c.c
@@ -4,16 +4,20 @@
 int main() {
   int i, sum = 0;
   int thread_sum[4];
+
   omp_set_num_threads(4);
+
 #pragma omp parallel
   {
     int ID = omp_get_thread_num();
     thread_sum[ID] = 0;
+
 #pragma omp for
     for (i = 0; i < 100; i++) {
       thread_sum[ID] += i;
     }
   }
+
   for (i = 0; i < 4; i++) {
     sum += thread_sum[i];
     printf("Thread %d: %d\n", i, thread_sum[i]);
